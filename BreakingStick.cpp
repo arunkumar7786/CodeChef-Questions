95dc1d77ede9d58d6ab46ac52762575ff8ae357c
Arun Kumar <arun8517.beaift24@chitkara.edu.in>
2025-07-07 18:27:59 +0530

    difficulity-596

---
+++ BreakingStick.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        int count = 0;
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+            while(arr[i]>1){
+                arr[i]-=1;
+                count++;
+            }
+        }
+        cout<<count<<endl;
+        
+        
+    }
+	// your code goes here
+
+}
